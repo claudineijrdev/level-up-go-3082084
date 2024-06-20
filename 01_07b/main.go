b0b192b80b3fb613abffcbbe1e5639c2887fd491
Claudinei C Jr <claudinei.junior.ext@allos.co>
2024-06-20 18:24:55 -0300

    Implement isBalanced function to check if an expression has balanced brackets

--- 01_07b/main.go
+++ 01_07b/main.go
@@ -5,14 +5,73 @@ import (
 	"log"
 )
 
+type operatorType int
+
+const (
+	openBrackets operatorType = iota
+	closedBrackets
+	otherOperator
+)
+
+var bracketPairs = map[rune]rune{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+}
+
+func getOperatorType(op rune) operatorType {
+	for ob, cb := range bracketPairs {
+		switch op {
+		case ob:
+			return openBrackets
+		case cb:
+			return closedBrackets
+		}
+	}
+
+	return otherOperator
+}
+
+type stack struct {
+	elems []rune
+}
+
+func (s *stack) push(elem rune) {
+	s.elems = append(s.elems, elem)
+}
+
+func (s *stack) pop() *rune {
+	if len(s.elems) == 0 {
+		return nil
+	}
+
+	n := len(s.elems) - 1
+	last := s.elems[n]
+	s.elems = s.elems[:n]
+
+	return &last
+}
+
 // isBalanced returns whether the given expression
 // has balanced brackets.
 func isBalanced(expr string) bool {
-	panic("NOT IMPLEMENTED")
+	s := stack{}
+	for _, e := range expr {
+		switch getOperatorType(e) {
+		case openBrackets:
+			s.push(e)
+		case closedBrackets:
+			last := s.pop()
+			if last == nil || bracketPairs[*last] != e {
+				return false
+			}
+		}
+	}
+	return len(s.elems) == 0
 }
 
 // printResult prints whether the expression is balanced.
-func printResult(expr string, balanced bool){ 
+func printResult(expr string, balanced bool) {
 	if balanced {
 		log.Printf("%s is balanced.\n", expr)
 		return
